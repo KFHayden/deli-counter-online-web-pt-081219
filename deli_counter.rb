f083ff0325317e433d42c731750b09d0b10a24c5
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-24 03:11:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty? == true
+    puts "The line is currently empty."
+  else
+    array = katz_deli.each_with_index.map do |place_no, customer|
+      "#{customer + 1}. #{place_no}"
+    end
+    puts "The line is currently: #{array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push name
+  counter = 1
+  
+  while counter <= katz_deli.length
+  puts "Welcome, #{name}. You are number #{counter+1} in line."
+end
+end
