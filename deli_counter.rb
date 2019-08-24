c5bcbdb584962778d4d02ffa347f17e371ec1673
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-24 03:20:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if now_serving.length(katz_deli) == 0
+  if now_serving == 0
    puts "There is nobody waiting to be served!"
   end
 end
